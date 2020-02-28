6302c197e164e7aae964245e455b78d613ac119b
Oleg Tielushko <tielushko@mail.usf.edu>
2020-02-28 14:49:08 -0500

    Update memsim.c
    Added the modified READ.
    Now reads small amount of memory references, if they don't exist in memory, reads from disk and increment countRead,
    If a hit, skips to next memory reference.
    NO REPLACEMENT WAS IMPLEMENTED YET.

--- memsim.c
+++ memsim.c
@@ -14,12 +14,13 @@ void lru(char* filename);
 void fifo(char* filename); 
 void vms(char* filename); 
 
-struct Entries {
-    unsigned address;
-    char readwrite;
-    int valid;
-
-} entries;
+struct PageEntry {
+    unsigned VPN;
+    char rw;
+    int dirty; //0 - clean 1 - dirty
+    unsigned BASE;
+    unsigned BOUND;
+} PageEntry;
 
 int main (int argc, char* argv[]) {
     //making sure that there is a correct number of arguments in the execution of the problem. 
@@ -57,12 +58,6 @@ int main (int argc, char* argv[]) {
     char* simulationMode = argv[3];
     printf("simulation mode is %s\n", simulationMode);
     
-    //fscanf(fp, "%x %c", &addr, &rw);
-    //printf("%x %c\n", addr, rw);
-    //fscanf(fp, "%x %c", &addr, &rw);
-    //printf("%x %c\n", addr, rw);
-    
-    
     if (strcmp(simulationMode, "LRU") == 0 || strcmp(simulationMode, "lru") == 0) {
         lru(filename);
     } else if (strcmp(simulationMode, "FIFO") == 0|| strcmp(simulationMode, "fifo") == 0) {
@@ -95,12 +90,77 @@ void rdm(char* filename) {
         exit(0);
     }
       
-    char* rw; 
+    char rw; 
     unsigned addr;
-    
+    int i = 0;
+    char READ = 'R';
+    char WRITE = 'W';
+    char* temp;
+    bool found; 
+    struct PageEntry RAM[nframes];
+    int k;
+
+    for (k = 0; k < nframes; k++) { //we need to initialize RAM as NULL all the way through 
+        RAM[k].VPN = 0;
+        RAM[k].rw = 0;
+        RAM[k].dirty = 0; //initialize dirty bit to zero
+    }
+
     while (fscanf(fp, "%x %c", &addr, &rw) != EOF) {
-        printf("%x %c\n", addr, rw);
+        //printf("%x %c\n", addr, rw);
     
+        //direct the flow if the R is seen
+        int j;
+        if(rw == 'R') { //works
+            for (j = 0; j < nframes; j++) { //scanning the RAM to find it in memory.
+                //if found need to break and set found to true;
+                unsigned readVPN = addr/4096;
+                if (readVPN == RAM[j].VPN) {
+                    printf("Reading from ram. HIT\n");
+                    found = true;
+                    break;//this is a hit we dont need to do anything //we can break.
+                } else {
+                    found = false;
+                }
+            }
+
+            if (!found) { //in case the page wasn't found in the RAM, we need to load it in MEMORY
+                int l; 
+                for (int l = 0; l < nframes; l++) {
+                    if (RAM[l].VPN == 0)  { //load the first empty page in RAM with information from the disk.
+                        RAM[l].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+                        RAM[l].rw = rw; //rw part
+                        RAM[l].dirty = 0; //initialize dirty bit to zero
+                        RAM[l].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+                        RAM[l].BOUND = RAM[l].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+                        countReads++;
+                        printf("%x %c %d %x %x\n", RAM[l].VPN, RAM[l].rw, RAM[l].dirty, RAM[l].BASE, RAM[l].BOUND);
+                        break;
+                    }
+                }
+            }
+        } //end if Read
+
+        if (rw == 'W') {
+
+        }
+        /*
+        RAM[i].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+        RAM[i].rw = rw; //rw part
+        RAM[i].dirty = 0; //initialize dirty bit to zero
+        RAM[i].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+        RAM[i].BOUND = RAM[i].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+        */
+        //printf("%x %c %d %x %x\n", RAM[i].VPN, RAM[i].rw, RAM[i].dirty, RAM[i].BASE, RAM[i].BOUND); //the test to make sure it works correctly proven itself.
+        //i++;
+        //where the population starts
+        
+        //for R if it is not in RAM -> reads the process from disk (readCount++) 
+          //writes the process into the RAM and marks it as "dirty"
+          
+        //for W
+          //if (have space in RAM) -> populate it into the RAM and mark the page as dirty. 
+          //else -> page replacement alogrithm. if page under replacement is dirty, numberWrites to disk is ++ 
     }
     
     fclose(fp);
