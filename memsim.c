acb9e33809066b20efcb955f90dce84d7c9aae36
acanaan97 <44300613+acanaan97@users.noreply.github.com>
2020-03-02 19:38:21 -0500

    minor updates // LRU finished // FIFO added

--- memsim.c
+++ memsim.c
@@ -4,8 +4,8 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <time.h>
-#include <float.h> //for DBL_MAX
-#define INT_MAX 2147483647
+// #include <float.h> //for DBL_MAX
+// #define INT_MAX 2147483647
 
 int nframes;
 int countReads = 0;
@@ -410,18 +410,23 @@ void lru(char* filename) {
     RAMQ->frame_table = (struct frame *)malloc(sizeof(struct frame) * nframes);
     RAMQ->size = nframes;
     RAMQ->index = 0;
+    // initialize our frame table
+    for (int z = 0; z < nframes; z++) {
+        RAMQ->frame_table[z].dirty = 0;
+        RAMQ->frame_table[z].address = 0;
+    }
     
     while (fscanf(fp, "%x %c", &addr, &rw) != EOF) {
         // Record data of current trace
         struct frame *current;
         current = (struct frame *)malloc(sizeof(struct frame));
         current->address = addr/4096;
-        current->dirty = 0;
+        current->dirty = 0; //initialize
         lines++; // Increment trace counter
 
-        // if (debug == true) { ***********************************************************************************
-        //     printf("Page request for %x, operation requested: %c\n", current->address, rw);
-        // }
+        if (debug == true) {
+            printf("Page request for %x, operation requested: %c\n", current->address, rw);
+        }
 
         // SECTION 1: WHEN TRACE IS A 'W' REQUEST
         if (rw == 'W' || rw == 'w') {
@@ -448,9 +453,9 @@ void lru(char* filename) {
 
             }
 
-            // if (debug = true) { *********************************************************************************************************
-            //     printf("RAMQ has been appended with trace: Address %x       Request Type: %c\n", current->address, rw);
-            // }
+            if (debug == true) { 
+                printf("RAMQ has been appended with trace: Address %x       Request Type: %c\n", current->address, rw);
+            }
 
         }
         // SECTION 2: TRACE IS A 'R' REQUEST
@@ -474,22 +479,22 @@ void lru(char* filename) {
                 countReads++; // Increment reads, have to go to disk to find this frame
                 //Don't need to update curr->dirty here, request was not a 'W'
                 insert(RAMQ, current); // Standard insert, if RAMQ is full the LRUmove has already occured in case 1, otherwise RAMQ not full and we can insert regularly.
-
+                
             }
 
-            // if (debug = true) { ******************************************************************************************
-            //     printf("RAMQ has been appended with trace: Address %x       Request Type: %c\n", current->address, rw);
-            // }
+            if (debug == true) { 
+                printf("RAMQ has been appended with trace: Address %x       Request Type: %c\n", current->address, rw);
+            }
 
         }
-        // if (debug == true) { // print our frame table for debug display ****************************************************************************
-        //     printf("PRINTING TABLE FOR DISPLAY\n");
-        //     for (int p = 0; p < nframes; p++){
-        //         printf("Index in Queue: %d    VPN: %x   Dirty: %d\n", p, RAMQ->frame_table[p].address, RAMQ->frame_table[p].dirty);
-        //     }
-        // }
+        if (debug == true) { // print our frame table for debug display 
+            printf("PRINTING TABLE FOR DISPLAY\n");
+            for (int p = 0; p < nframes; p++){
+                printf("Index in Queue: %d    VPN: %x   Dirty: %d\n", p, RAMQ->frame_table[p].address, RAMQ->frame_table[p].dirty);
+            }
+        }
     } // end of while loop
-    printf("Traces processed: %d    Disk Reads: %d      Disk Writes: %d\n", lines, countReads, countWrites);
+    printf("Total memory frames: %d\nEvents in trace: %d\nTotal Disk Writes: %d\nTotal Disk Reads: %d\n", nframes, lines, countWrites, countReads);
     free(RAMQ->frame_table);
     free(RAMQ);
     fclose(fp);
@@ -504,15 +509,242 @@ void fifo(char* filename) {
         exit(0);
     }
       
-    char* rw; 
+    char rw; 
     unsigned addr;
+    char READ = 'R';
+    char WRITE = 'W';
+    //char* temp;
+    bool found;
+    bool full = false;
+    struct PageEntry RAM[nframes];
+    int k;
+    int fifoIndex = 0; 
+
+    //The RAM will also serve as our FIFO queue, as we are filling up ram top to bottom, keeping track of proper indeces.
+    for (k = 0; k < nframes; k++) { //we need to initialize RAM as NULL all the way through 
+        RAM[k].VPN = 0;
+        RAM[k].rw = 0;
+        RAM[k].dirty = 0; //initialize dirty bit to zero
+    }
+
     
     while (fscanf(fp, "%x %c", &addr, &rw) != EOF) {
-        printf("%x %c\n", addr, rw);
+        nEvents++;
     
+        int j;
+        //direct the flow if the R is seen
+        if(rw == 'R') { //works
+            for (j = 0; j < nframes; j++) { //scanning the RAM to find it in memory (if the process is already in memory).
+                //if found need to break and set found to true;
+                unsigned readVPN = addr/4096;
+                if (readVPN == RAM[j].VPN) {
+                    //add condition to print only for debug mode 
+                    RAM[j].dirty = 0;     
+                    if(debug == true) {                   
+                        printf("Reading %x memory reference from page %x in RAM. HIT. Dirty: %d\n", addr, readVPN, RAM[j].dirty);
+                    }
+                    found = true;
+                    break;//this is a hit we dont need to do anything //we can break.
+                } else {
+                    found = false;
+                }
+            }
+
+            if (!found) { //in case the page wasn't found in the RAM, we need to load it in MEMORY
+                int m;
+                int emptypages = 0;
+
+                for (m = 0; m < nframes; m++) { //UPDATE: Processes the empty correctly. 
+                    if (RAM[m].VPN == 0)        
+                        emptypages++;
+                }
+                
+                if (emptypages == 0)
+                    full = true;
+                else 
+                    full = false;
+                
+                int l;
+                 
+                for (l = 0; l < nframes; l++) { //run for loop to check for empty frames.
+                    if (RAM[l].VPN == 0)  { //load the first empty page in RAM with information from the disk.
+                        RAM[l].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+                        RAM[l].rw = rw; //rw part
+                        RAM[l].dirty = 0; //initialize dirty bit to zero
+                        RAM[l].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+                        RAM[l].BOUND = RAM[l].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+                        countReads++;
+                        //add condition to print only for debug mode
+                        if(debug == true)
+                            printf("\nReading from disk and placing the following memory reference: %x into an empty space %d in RAM. VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", addr, l, RAM[l].VPN, RAM[l].rw, RAM[l].dirty, RAM[l].BASE, RAM[l].BOUND);
+                        break;
+                    }
+                } 
+                
+                //otherwise if the entire table is full, we need to use a page replacement algorithm here. 
+                //THIS IS THE JUICE OF ALGORITHM
+                if (full) {
+                    
+                    //condition to check which index to be eliminated.
+                    if(debug == true) {
+                        printf("\nIndex to be eliminated %d\n", fifoIndex);
+                        printf("Page to be eliminated: VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", RAM[fifoIndex].VPN, RAM[fifoIndex].rw, RAM[fifoIndex].dirty, RAM[fifoIndex].BASE, RAM[fifoIndex].BOUND);
+                    }
+
+                    if (RAM[fifoIndex].dirty == 0) {
+                        //your casual replacement.
+                            if(debug == true)
+                                printf("\nEliminating a clean page %x, no write to the disk required.\n", RAM[fifoIndex].VPN);
+                            RAM[fifoIndex].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+                            RAM[fifoIndex].rw = rw; //rw part
+                            RAM[fifoIndex].dirty = 0; //initialize dirty bit to zero
+                            RAM[fifoIndex].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+                            RAM[fifoIndex].BOUND = RAM[fifoIndex].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+                            countReads++;
+                            //add condition to print only for debug mode
+                            if(debug == true) 
+                                printf("\nPage replacement for space %d in RAM. Current VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", fifoIndex, RAM[fifoIndex].VPN, RAM[fifoIndex].rw, RAM[fifoIndex].dirty, RAM[fifoIndex].BASE, RAM[fifoIndex].BOUND);
+                    } else {
+                    //if(RAM[fifoIndex].dirty == 1) {
+                        if(debug == true)
+                            printf("\nEliminating a dirty page %x, requires a write to the disk.\n", RAM[fifoIndex].VPN);
+                        countWrites++;
+                        //think if this should get replaced...
+                        RAM[fifoIndex].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+                        RAM[fifoIndex].rw = rw; //rw part
+                        RAM[fifoIndex].dirty = 0; //initialize dirty bit to zero
+                        RAM[fifoIndex].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+                        RAM[fifoIndex].BOUND = RAM[fifoIndex].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+                        countReads++;
+                        //add condition to print only for debug mode
+                        if(debug == true) 
+                            printf("\nPage replacement for space %d in RAM. Current VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", fifoIndex, RAM[fifoIndex].VPN, RAM[fifoIndex].rw, RAM[fifoIndex].dirty, RAM[fifoIndex].BASE, RAM[fifoIndex].BOUND);
+                        
+                    }
+
+                    if (fifoIndex == nframes-1) {
+                        fifoIndex = 0;
+                    } else {
+                        fifoIndex++;
+                    }
+
+                }   
+            }
+        } //end if Read
+
+        if (rw == 'W') {
+            //check if the page already exists in the ram, then it will just be written on top of it. 
+            for (j = 0; j < nframes; j++) { //scanning the RAM to find it in memory (if the process is already in memory).
+                //if found need to break and set found to true;
+                unsigned writeVPN = addr/4096;
+                if (writeVPN == RAM[j].VPN) {
+                    //add condition to print only for debug mode 
+                    RAM[j].dirty = 1;
+                    if(debug == true) {                   
+                        printf("Rewriting the page: %x memory reference onto the page %x in RAM.\n", addr, writeVPN);
+                    }   
+                    found = true;
+                    //add the fifoIndex??? maybe...
+                    break;//this is a hit we dont need to do anything //we can break.
+                } else {
+                    found = false;
+                }
+            }
+
+            //if the page is not located in the ram, we need to find an empty space for it to fit into the ram
+            if (!found) {
+                int m;
+                int emptypages = 0;
+
+                for (m = 0; m < nframes; m++) { //UPDATE: Processes the empty correctly. 
+                    if (RAM[m].VPN == 0)        
+                        emptypages++;
+                }
+                    
+                if (emptypages == 0)
+                    full = true;
+                else 
+                    full = false;
+
+                if (!full) {
+                    int l;
+                    for (l = 0; l < nframes; l++) { //run for loop to check for empty frames.
+                        if (RAM[l].VPN == 0)  { //if we find the first empty page, we need to write into it
+                            RAM[l].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+                            RAM[l].rw = rw; //rw part
+                            RAM[l].dirty = 1; //if the operation is write, dirty bit must be 1 then.
+                            RAM[l].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+                            RAM[l].BOUND = RAM[l].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+                            //add condition to print only for debug mode
+                            if(debug == true)
+                                printf("\nWriting the following memory reference: %x into an empty space %d in RAM. VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", addr, l, RAM[l].VPN, RAM[l].rw, RAM[l].dirty, RAM[l].BASE, RAM[l].BOUND);
+                            break;
+                        }
+                    }
+                } else { //ALGORITHM JUICE
+                    
+                    //condition to check which index to be eliminated.
+                    if(debug == true) {
+                        printf("\nIndex to be eliminated %d\n", fifoIndex);
+                        printf("Page to be eliminated: VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", RAM[fifoIndex].VPN, RAM[fifoIndex].rw, RAM[fifoIndex].dirty, RAM[fifoIndex].BASE, RAM[fifoIndex].BOUND);
+                    }
+
+                    if (RAM[fifoIndex].dirty == 0) {
+                        if(debug == true)
+                            printf("\nEliminating a clean page %x, no write to the disk required.\n", RAM[fifoIndex].VPN);
+                        //rewriting the page
+                        RAM[fifoIndex].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+                        RAM[fifoIndex].rw = rw; //rw part
+                        RAM[fifoIndex].dirty = 1; //initialize dirty bit to one as the command was W
+                        RAM[fifoIndex].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+                        RAM[fifoIndex].BOUND = RAM[fifoIndex].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+                        
+                        //add condition to print only for debug mode
+                        if(debug == true) 
+                            printf("\nPage replacement for space %d in RAM. Current VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", fifoIndex, RAM[fifoIndex].VPN, RAM[fifoIndex].rw, RAM[fifoIndex].dirty, RAM[fifoIndex].BASE, RAM[fifoIndex].BOUND); 
+                    } else {
+                    //over here it bugs out where if the dirty index was switched from 0 to 1 it would perform two page replacements. 
+                    //if(RAM[fifoIndex].dirty == 1) {
+                        if(debug == true)
+                            printf("\nEliminating a dirty page %x, requires a write to the disk.\n", RAM[fifoIndex].VPN);
+                        countWrites++;
+                        //rewriting the page.
+                        RAM[fifoIndex].VPN = addr/4096; //we need to divide by 4096 to eliminate the page offset. (in hex 4096 is equal to 0x1000))
+                        RAM[fifoIndex].rw = rw; //rw part
+                        RAM[fifoIndex].dirty = 1; //initialize dirty bit to one as the command was W
+                        RAM[fifoIndex].BASE = addr/4096 * 0x1000; //getting the BASE of the single page. Many of the accesses will be within the same page
+                        RAM[fifoIndex].BOUND = RAM[fifoIndex].BASE + 0xfff; //getting the BOUND of the page. if you add +1, this will technically be the different page.
+                        
+                        //add condition to print only for debug mode
+                        if(debug == true) 
+                            printf("\nPage replacement for space %d in RAM. Current VPN: %x RW: %c Dirty: %d Base: %x Bound: %x\n", fifoIndex, RAM[fifoIndex].VPN, RAM[fifoIndex].rw, RAM[fifoIndex].dirty, RAM[fifoIndex].BASE, RAM[fifoIndex].BOUND);
+                        
+                    }       
+                        if (fifoIndex == nframes-1) {
+                        fifoIndex = 0;
+                        } else {
+                        fifoIndex++;
+                        }
+                                 
+                }
+            }
+            //if empty space is not found, we need to apply the page replacement algorithm to select which page to evict.
+                //if page under eviction is dirty, we need to save it to the disk -> diskWrites++
+                //if page under eviction is !dirty, we just replace it. no need to increment anything. 
+            
+        }
+    }
+
+    if (debug == true) {
+        //The final Ram 
+        printf("The final RAM contents are the following: \n");
+        for (int z = 0; z < nframes; z++) {
+            printf("Slot: %d, VPN: %x, Dirty: %d\n", z, RAM[z].VPN, RAM[z].dirty);
+        }
     }
     fclose(fp);
 }
+
 void vms(char* filename) {
     FILE *fp;
     fp = fopen(filename, "r"); // Open our file.
